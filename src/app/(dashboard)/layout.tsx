c900fa75a8a896edc46e4ac841b09a05f47772ae
Keshav Mehndiratta <mehndirattakeshav@gmail.com>
2026-03-11 23:25:27 -0400

    new layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Sidebar } from "@/components/ui/sidebar";
+import { CommandPalette } from "@/components/ui/command-palette";
 import { StoreProvider } from "@/lib/store";
 
 export default function DashboardLayout({
@@ -14,6 +15,7 @@ export default function DashboardLayout({
         <Sidebar />
         <main className="flex-1 overflow-auto">{children}</main>
       </div>
+      <CommandPalette />
     </StoreProvider>
   );
 }
